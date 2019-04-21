b016f7dce9a093e25ec051067ee303adccb71bc6
Rajat Mhetre <mhetrerajat@gmail.com>
2019-04-21 21:12:28 +0530

    add validation method in json parser

--- nest.py
+++ nest.py
@@ -14,7 +14,24 @@ class Parser(object):
         self.validate()
 
     def validate(self):
-        pass
+        # Validate array
+        if not isinstance(self.data, list):
+            raise TypeError(
+                "Input should be a JSON Array of JSON Array Objects")
+        elif not len(self.data):
+            raise ValueError("Input JSON Array can not be empty")
+
+        # Validate array items
+        for item in self.data:
+            if not isinstance(item, dict):
+                raise TypeError(
+                    "Input should be a JSON Array of JSON Array Objects")
+
+            invalid_levels = set(self.nesting_levels).difference(item.keys())
+            if invalid_levels:
+                raise ValueError(
+                    "Levels passed as argument does not present in data : {0}".
+                    format(invalid_levels))
 
     def parse(self):
         leaf_names = self.nesting_levels
