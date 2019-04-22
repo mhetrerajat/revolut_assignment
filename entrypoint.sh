dc3efd7aeb56dd50604d43e3f271ff31f8e6088d
Rajat Mhetre <mhetrerajat@gmail.com>
2019-04-22 20:22:44 +0530

    add initdb command to add default users and deposits

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 flask db migrate
 flask db upgrade
+flask initdb
 flask run --host=0.0.0.0
 # exec gunicorn -b :5000 \
 #     --name revolut_api \
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from dotenv import load_dotenv
 from config import BASE_DIR
@@ -16,6 +17,31 @@ from app import create_app, db
 app = create_app(os.getenv('FLASK_ENV') or 'default')
 app.app_context().push()
 migrate = Migrate(app, db)
+
+
+@app.cli.command()
+def initdb():
+    from app.models import User, Deposit
+
+    # Create admin user
+    user = User.query.filter_by(username="admin").first()
+    if not user:
+        user = User(username="admin", password="admin")
+        db.session.add(user)
+        db.session.commit()
+
+    # Add deposits from example_input.json
+    deposits = Deposit.query.count()
+    if deposits == 0:
+        f = open(os.path.join(BASE_DIR, 'example_input.json'))
+        data = json.loads(f.detach().read())
+        for item in data:
+            item.update({'user': user.id})
+            deposit = Deposit(**item)
+            db.session.add(deposit)
+
+        db.session.commit()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
