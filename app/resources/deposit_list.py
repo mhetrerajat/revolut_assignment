29186e5565bcc1dd65a238fc59cfe4ab89567ed6
Rajat Mhetre <mhetrerajat@gmail.com>
2019-04-22 11:29:16 +0530

    add resources for registration and deposit_list

---
+++ app/resources/deposit_list.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+from flask.json import jsonify
+
+from app import auth, db
+
+
+class DepositList(Resource):
+
+    decorators = [auth.login_required]
+
+    def get(self):
+        return jsonify({
+            'data': None,
+            'message': 'Get Deposit List',
+            'status': 'success'
+        })
---
+++ app/resources/register.py
@@ -0,0 +1,29 @@
+from flask import abort
+from flask.json import jsonify
+from flask_restful import Resource, reqparse
+
+from app import db
+from app.models import User
+
+
+class Register(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type=str, required=True)
+        self.reqparse.add_argument('password', type=str, required=True)
+
+        super(Register, self).__init__()
+
+    def post(self):
+        args = self.reqparse.parse_args()
+
+        user = User.query.filter_by(username=args.username).first()
+        if user:
+            abort(400)
+
+        user = User(username=args.username)
+        user.hash_password(args.password)
+        db.session.add(user)
+        db.session.commit()
+
+        return {'username': args.username, 'message': 'Account Created!'}
